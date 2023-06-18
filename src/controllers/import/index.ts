c68cf330c1d779620b78207074aa393c3913501b
Corolea Robert-Cozmin <robert.corolea@gmail.com>
2023-06-18 23:45:25 +0300

    feat: Added job service to import controller

--- src/controllers/import/index.ts
+++ src/controllers/import/index.ts
@@ -1,11 +1,13 @@
 import {NextFunction, Request, Response} from "express";
+import {createJob} from "../../services/jobService";
+import {JOB_TYPE} from "../../services/jobService/@constants";
 
 export const getImport = (req: Request, res: Response, next: NextFunction) => {
   //TODO: Add logic
   res.status(200).send('Success')
 }
 
-export const postImport = (req: Request, res: Response, next: NextFunction) => {
-  //TODO: Add logic
+export const postImport = async (req: Request, res: Response, next: NextFunction) => {
+  await createJob(req.body, JOB_TYPE.IMPORT)
   res.status(200).send('Success')
 }
