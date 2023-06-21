31e93ccfd2303df83f99a0b637a7a557c078b046
Corolea Robert-Cozmin <robert.corolea@gmail.com>
2023-06-21 18:40:00 +0300

    feat: Implemented apply function

--- operation-collision/src/index.ts
+++ operation-collision/src/index.ts
@@ -1,1 +1,10 @@
+import {Operation} from "./module/Operation";
+
 console.log('Hello World!')
+
+const s = "abcdefg";
+const op1 = new Operation([{ skip: 1 }, { insert: "FOO" }]);
+const op2 = new Operation([{ skip: 3 }, { insert: "BAR" }]);
+
+console.log(op1.apply(s))
+console.log(op2.apply(s))
--- operation-collision/src/module/Operation/index.ts
+++ operation-collision/src/module/Operation/index.ts
@@ -3,9 +3,27 @@ import {OperationEditQueue} from "./@types";
 
 export class Operation {
   private cursor: number = 0
-  editQueue: OperationEditQueue
+  editList: OperationEditQueue
 
-  constructor(editQueue: OperationEditQueue) {
-    this.editQueue = editQueue
+  constructor(editList: OperationEditQueue) {
+    this.editList = editList
+  }
+
+  apply(text: string) {
+    let resultText = text
+
+    for (const editItem of this.editList) {
+      if ('skip' in editItem) {
+        // Assumption here is made that the skip operation is incremental
+        // Similar how a cursor is moved a number of character from left to right
+        this.cursor += editItem.skip
+      } else if ('insert' in editItem) {
+        resultText = resultText.slice(0, this.cursor) + editItem.insert + resultText.slice(this.cursor);
+      } else if ('delete' in editItem) {
+        resultText = resultText.slice(this.cursor, editItem.delete)
+      }
+    }
+
+    return resultText
   }
 }
