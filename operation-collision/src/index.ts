3d3372d3bd4ae6ef04bcd0378318508867390ec9
Corolea Robert-Cozmin <robert.corolea@gmail.com>
2023-06-22 12:22:52 +0300

    feat: Refactored combination process and added comments

--- operation-collision/src/index.ts
+++ operation-collision/src/index.ts
@@ -1,14 +1,42 @@
 import {Operation} from "./module/Operation";
 
-const s = "abcdefg";
-const op1 = new Operation([{ skip: 1 }, { insert: "FOO" }]);
-const op2 = new Operation([{ skip: 3 }, { insert: "BAR" }]);
-const op3 = new Operation([{ skip: 2 }, { delete: 2 }]);
+// const s = "abcdefg";
+// const op1 = new Operation([{ skip: 1 }, { insert: "FOO" }]);
+// const op2 = new Operation([{ skip: 3 }, { insert: "BAR" }]);
+//
+//
+// const combined1 = Operation.combine(op1, op2);
+// const combined2 = Operation.combine(op2, op1);
+//
+// console.log(combined1.apply(s))
+// console.log(combined1)
+// console.log(combined2.apply(s))
+// console.log(combined2)
 
-console.log(op1.apply(s))
-console.log(op2.apply(s))
-console.log(op3.apply(s))
+
+const s = "1234567";
+
+// 1F23456O7
+const op1 = new Operation([
+  {skip: 1}, {insert: "(A1)"},
+  {skip: 3}, {insert: "(B1)"},
+]);
+// 123B45A67
+const op2 = new Operation([
+  {skip: 3}, {insert: "(A2)"},
+  {skip: 3}, {insert: "(B2)"},
+]);
+// 1F23B45A6O7
+
+// console.log(op1.apply(s))
+// console.log(op2.apply(s))
 
 const combined1 = Operation.combine(op1, op2);
-const combined2 = op1.combine(op2)
+const combined2 = Operation.combine(op2, op1);
+
 console.log(combined1.apply(s))
+// console.log(combined1)
+console.log(combined2.apply(s))
+// console.log(combined2)
+
+
--- operation-collision/src/module/Operation/index.ts
+++ operation-collision/src/module/Operation/index.ts
@@ -2,8 +2,7 @@ import {OperationEditQueue} from "./@types";
 
 
 export class Operation {
-  private cursor: number = 0
-  editList: OperationEditQueue
+  private editList: OperationEditQueue
 
   constructor(editList: OperationEditQueue) {
     this.editList = editList
@@ -11,33 +10,128 @@ export class Operation {
 
   combine(operation: Operation) {
     // Assumption here is made that the operation from the second item go after the first one
-    this.editList = [...this.editList, ...operation.editList]
+    this.editList = Operation.combineOperationsEditList(this.editList, operation.editList)
     return this
   }
 
-  static combine (operation1: Operation, operation2: Operation) {
-    return new Operation([...operation1.editList, ...operation2.editList])
+  static combine(operation1: Operation, operation2: Operation) {
+    // Assumption here is made that the operation from the second item go after the first one
+    const combinedEdit = this.combineOperationsEditList(operation1.editList, operation2.editList)
+    return new Operation(combinedEdit)
   }
 
   apply(text: string) {
+    let cursor = 0
     let resultText = text
 
     for (const editItem of this.editList) {
       if ('skip' in editItem) {
         // Assumption here is made that the skip operation is incremental
-        // Similar how a cursor is moved a number of character from left to right
-        this.cursor += editItem.skip
+        // Similar how an editor cursor is moved a number of character from left to right
+        cursor += editItem.skip
       } else if ('insert' in editItem) {
-        resultText = resultText.slice(0, this.cursor) + editItem.insert + resultText.slice(this.cursor);
+        resultText = resultText.slice(0, cursor) + editItem.insert + resultText.slice(cursor);
         // From the example I saw that after an insert the cursor moved by the number of characters in insert
-        this.cursor += editItem.insert.length
+        cursor += editItem.insert.length
       } else if ('delete' in editItem) {
-        const firstPart = resultText.substring(0, this.cursor)
-        const secondPart = resultText.substring(this.cursor + editItem.delete)
+        const firstPart = resultText.substring(0, cursor)
+        const secondPart = resultText.substring(cursor + editItem.delete)
         resultText = firstPart + secondPart
       }
     }
 
     return resultText
+  }
+
+  private static combineOperationsEditList(editList1: OperationEditQueue, editList2: OperationEditQueue) {
+    let resultEditList: OperationEditQueue = [...editList1]
+
+    const firstItem = editList2[0]
+    const firstSkip = 'insert' in firstItem || 'delete' in firstItem ? 0 : firstItem["skip"]
+    const {skipDelta, insertDelta} = this.getSkipAndInsertDelta(editList1, firstSkip)
+    const newSecondList = this.createSecondList(editList2, skipDelta, insertDelta)
+
+    return [...resultEditList, ...newSecondList]
+  }
+
+  // This method is used in the operation combination process
+  // We iterate over the first list of edits and get the following information
+  // skipDelta - this is used to modify the first skip edit in the second list to the correct position based on the final value from the first list
+  // insertDelta - this is on object contain information of on which index in the text and how many characters were added. We use the following format
+  // {[number_string_format]: number} - where the property represents the index on the string where a substring was added and the value is the length of the substring
+  private static getSkipAndInsertDelta(editList: OperationEditQueue, firstSkip: number) {
+    let skipDelta = 0
+    let insertDelta = {}
+    let lefIndex = 0
+
+    for (const editItem of editList) {
+      if (firstSkip > skipDelta) {
+        skipDelta += editItem['skip'] || 0
+      } else {
+        skipDelta += editItem['skip'] || editItem['insert']?.length || 0
+      }
+
+      if ('skip' in editItem) {
+        lefIndex += editItem['skip']
+      }
+
+      if ('insert' in editItem) {
+        if (!insertDelta[lefIndex]) {
+          insertDelta[lefIndex] = 0
+        }
+
+        insertDelta[lefIndex] += editItem['insert']?.length
+      }
+    }
+
+    return {
+      skipDelta,
+      insertDelta
+    }
+  }
+
+  // This method is used in the operation combination process
+  // Using the information from the first list we create a new second list with the skip edits modified
+  private static createSecondList(editList: OperationEditQueue, skipDelta: number, insertDelta: Record<string, number>) {
+    let resultEditList: OperationEditQueue = []
+    const firstItem = editList[0]
+    const firstSkip = 'insert' in firstItem || 'delete' in firstItem ? 0 : firstItem["skip"]
+    let lefIndex = 0
+
+    for (const editItem of editList) {
+      if ('skip' in editItem) {
+        lefIndex += editItem['skip']
+
+        if (editItem === firstItem) {
+          resultEditList = [...resultEditList, {skip: editItem['skip'] - skipDelta}]
+        } else {
+          const newSkipDelta = this.calculateNewSkipDelta(insertDelta, firstSkip, lefIndex)
+
+          resultEditList = [...resultEditList, {skip: editItem['skip'] + newSkipDelta}]
+        }
+
+      } else {
+        if (editItem === firstItem) {
+          resultEditList = [...resultEditList, {skip: -skipDelta}, editItem]
+        } else {
+          resultEditList = [...resultEditList, editItem]
+        }
+      }
+    }
+
+    return resultEditList
+  }
+
+  // Calculate how much to move the skip field of non-first items for the second list
+  private static calculateNewSkipDelta(insertDelta: Record<string, number>, firstSkip: number, lefIndex: number) {
+    const addDeltaKeys = Object.keys(insertDelta).map(key => Number(key))
+    const validKeys = addDeltaKeys.filter(key => (key >= firstSkip && key < lefIndex))
+    let newSkipDelta = 0
+
+    for (const validKey of validKeys) {
+      newSkipDelta += insertDelta[validKey]
+    }
+
+    return newSkipDelta
   }
 }
